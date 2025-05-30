70a0498930d301ebf244d9fb5d46aedcb59807d2
Abhishek Kumar <Abhishek.Kumar39@gds.ey.com>
2025-05-30 13:49:02 +0530

    End Section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,24 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule, CommonModule],
+  imports: [CommonModule, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
-  http = inject(HttpClient);
+  
+  private accountService = inject(AccountService);
   title = 'client';
-  users: any;
 
-    ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response
-    })
+
+  ngOnInit(): void {
+    this.setCurrentUser();
   }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  
 }
